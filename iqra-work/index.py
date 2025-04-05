38616716be377fd48b27f78f76860c992559dd1e
iqranaveed2060 <iqranaveed2060@gmail.com>
2025-04-05 13:22:04 +0500

    done from iqra

--- iqra-work/index.py
+++ iqra-work/index.py
@@ -1,9 +1,1 @@
-# Taking input from the user
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-# Calculating sum
-sum_result = num1 + num2
-
-# Displaying the result
-print("The sum of", num1, "and", num2, "is:", sum_result)
+print ("i love pakistan")
